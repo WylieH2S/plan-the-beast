52a07c58d1c1cc08857991977bbe97b02f2c2a9e
WylieH2SC <lobe.sphinx-08@icloud.com>
2025-05-14 11:39:04 -0400

    Plaitaria - 1.0
    My first official AI created program release. Allegedly this thing is cutting edge, first to market in it's wide audience, and completely 100% working for Satisfactory, with support for future games and features.

---
+++ Planitaria/Planitaria_Complete_Build/src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PlannerApp from './PlannerApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<PlannerApp />);
