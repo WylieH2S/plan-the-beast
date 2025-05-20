26fbcb73b2dc8c05348be40c1624c68eb8059f09
WylieH2SC <lobe.sphinx-08@icloud.com>
2025-05-20 18:05:28 -0400

    satisfactory

--- Planitaria/Gamepack.js
+++ Planitaria/Gamepack.js
@@ -3,17 +3,27 @@ export function loadGamepack(name) {
     return {
       name: "Satisfactory",
       stencils: [
-        { type: "Miner", role: "input" },
-        { type: "Smelter", role: "logic" },
-        { type: "Constructor", role: "output" }
+        { type: "Miner Mk.1", role: "input", power: 5, throughput: 60 },
+        { type: "Miner Mk.2", role: "input", power: 12, throughput: 120 },
+        { type: "Smelter", role: "logic", power: 4, throughput: 30 },
+        { type: "Constructor", role: "logic", power: 4, throughput: 20 },
+        { type: "Foundry", role: "logic", power: 16, throughput: 45 },
+        { type: "Storage Container", role: "output", power: 1, throughput: 60 },
+        { type: "Power Pole", role: "power", power: 0, throughput: 0 },
+        { type: "Biomass Burner", role: "power", power: -30, throughput: 0 },
+        { type: "Conveyor Belt Mk.1", role: "link", power: 0, throughput: 60 },
+        { type: "Conveyor Belt Mk.2", role: "link", power: 0, throughput: 120 },
+        { type: "Pipe", role: "link", power: 0, throughput: 300 }
       ]
     };
   }
+
   if (name === "empty") {
     return {
-      name: "Empty Test Pack",
+      name: "Empty Planitae",
       stencils: []
     };
   }
+
   return { name: "Unknown", stencils: [] };
 }
